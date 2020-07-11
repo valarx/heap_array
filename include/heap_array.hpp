43668c14a2f7256d27adb4081015e97a8ddd82e4
valarx <valar1989@gmail.com>
2020-07-12 00:47:57 +0200

    feat: Extend functionality
    Support the same interface as std::array. Add iterator support.

--- include/heap_array.hpp
+++ include/heap_array.hpp
@@ -3,8 +3,10 @@
 #include <cassert>
 #include <cstdint>
 #include <initializer_list>
+#include <iterator>
 #include <limits>
 #include <new>
+#include <stdexcept>
 #include <type_traits>
 #include <utility>
 
@@ -25,6 +27,136 @@ class heap_array final {
     return std::launder(reinterpret_cast<value_type *>(storage_pointer));
   }
 
+  class random_access_iterator final {
+    friend heap_array<T>;
+
+    explicit random_access_iterator(T *const ptr) noexcept : ptr_{ptr} {}
+
+    T *ptr_;
+
+  public:
+    random_access_iterator() noexcept : ptr_{} {};
+
+    using difference_type = std::int64_t;
+    using value_type = T;
+    using pointer = value_type *;
+    using reference = value_type &;
+    using const_reference = const value_type &;
+    using iterator_category = std::random_access_iterator_tag;
+
+    reference operator*() noexcept { return *ptr_; }
+
+    const_reference operator*() const noexcept { return *ptr_; }
+
+    reference operator[](const difference_type shift) noexcept {
+      return *(ptr_ + shift);
+    }
+
+    const_reference operator[](const difference_type shift) const noexcept {
+      return *(ptr_ + shift);
+    }
+
+    friend random_access_iterator &
+    operator++(random_access_iterator &iter) noexcept {
+      ++iter.ptr_;
+      return iter;
+    }
+
+    friend random_access_iterator operator++(random_access_iterator &iter,
+                                             int) noexcept {
+      auto retval = iter;
+      ++iter.ptr_;
+      return retval;
+    }
+
+    friend random_access_iterator &
+    operator--(random_access_iterator &iter) noexcept {
+      --iter.ptr_;
+      return iter;
+    }
+
+    friend random_access_iterator operator--(random_access_iterator &iter,
+                                             int) noexcept {
+      auto retval = iter;
+      --iter.ptr_;
+      return retval;
+    }
+
+    friend random_access_iterator &operator+=(random_access_iterator &iter,
+                                              const difference_type shift) {
+      iter.ptr_ += shift;
+      return iter;
+    }
+
+    friend random_access_iterator &operator-=(random_access_iterator &iter,
+                                              const difference_type shift) {
+      iter.ptr_ -= shift;
+      return iter;
+    }
+
+    friend random_access_iterator operator+(const random_access_iterator &iter,
+                                            const difference_type shift) {
+      return random_access_iterator{iter.ptr_ + shift};
+    }
+
+    friend random_access_iterator
+    operator+(const difference_type shift, const random_access_iterator &iter) {
+      return random_access_iterator{iter.ptr_ + shift};
+    }
+
+    friend random_access_iterator operator-(const random_access_iterator &iter,
+                                            const difference_type shift) {
+      return random_access_iterator{iter.ptr_ - shift};
+    }
+
+    friend difference_type operator-(const random_access_iterator &lhs,
+                                     const random_access_iterator &rhs) {
+      return static_cast<difference_type>(lhs.ptr_ - rhs.ptr_);
+    }
+
+    friend random_access_iterator
+    operator-(const difference_type shift, const random_access_iterator &iter) {
+      return random_access_iterator{iter.ptr_ - shift};
+    }
+
+    friend bool operator==(const random_access_iterator &lhs,
+                           const random_access_iterator &rhs) {
+      return lhs.ptr_ == rhs.ptr_;
+    }
+
+    friend bool operator!=(const random_access_iterator &lhs,
+                           const random_access_iterator &rhs) {
+      return !(lhs == rhs);
+    }
+
+    friend bool operator<(const random_access_iterator &lhs,
+                          const random_access_iterator &rhs) {
+      return lhs.ptr_ < rhs.ptr_;
+    }
+
+    friend bool operator>(const random_access_iterator &lhs,
+                          const random_access_iterator &rhs) {
+      return rhs.ptr_ < lhs.ptr_;
+    }
+
+    friend bool operator<=(const random_access_iterator &lhs,
+                           const random_access_iterator &rhs) {
+      return !(lhs.ptr_ > rhs.ptr_);
+    }
+
+    friend bool operator>=(const random_access_iterator &lhs,
+                           const random_access_iterator &rhs) {
+      return !(lhs.ptr_ < rhs.ptr_);
+    }
+  };
+
+  void reset_storage() noexcept {
+    for (size_type i{}; i < size_; ++i) {
+      to_value_type_pointer(storage_ + i)->~value_type();
+    }
+    delete[] storage_;
+  }
+
 public:
   using value_type = T;
   using size_type = SizeType;
@@ -32,56 +164,97 @@ public:
   using const_reference = const value_type &;
   using pointer = value_type *;
   using const_pointer = const value_type *;
-  using iterator = void; // TODO implement this
+  using iterator = random_access_iterator; // TODO implement this
   using const_iterator = void;
   using reverse_iterator = void;
   using const_reverse_iterator = void;
 
   explicit heap_array(const size_type size) : storage_{}, size_{size} {
-    assert(size_ > 0);
-
-    storage_ = new storage_type[size_];
+    if (size_ > 0) {
+      storage_ = new storage_type[size_];
+    }
   }
 
   heap_array(std::initializer_list<value_type> init)
       : storage_{}, size_{init.size()} {
     assert(init.size() <= std::numeric_limits<size_type>::max());
-
-    storage_ = new storage_type[init.size()];
-    size_type idx{0};
-    for (auto &val : init) {
-      new (storage_ + idx) value_type(val); // TODO is this correct?
-      ++idx;
+    if (size_ > 0) {
+      storage_ = new storage_type[init.size()];
+      size_type idx{0};
+      for (auto &val : init) {
+        new (storage_ + idx) value_type(val); // TODO is this correct?
+        ++idx;
+      }
     }
   }
 
-  template <typename U, typename OtherSizeType>
-  heap_array(const heap_array<U, OtherSizeType> &other) {
+  template <typename OtherSizeType>
+  heap_array(const heap_array<T, OtherSizeType> &other) {
     assert(other.size_ <= std::numeric_limits<size_type>::max());
     // TODO implement this
   }
 
-  const value_type &operator[](const size_type pos) const {
-    assert(pos < size_);
-    // note: needs std::launder as of C++17
-    return *to_value_type_pointer(&storage_[pos]);
-  }
-
-  value_type &operator[](const size_type pos) {
-    assert(pos < size_);
-    // note: needs std::launder as of C++17
-    return *to_value_type_pointer(&storage_[pos]);
-  }
-
-  size_type size() const { return size_; }
-
-  ~heap_array() {
-    // TODO use std::launder
-    for (size_type i{}; i < size_; ++i) {
-      to_value_type_pointer(storage_ + i)->~value_type();
+  const_reference at(const size_type pos) const {
+    if (pos >= size_) {
+      throw std::out_of_range("Tryin to access element which is out of range");
     }
-    delete[] storage_;
+    return *to_value_type_pointer(storage_ + pos);
   }
+
+  reference at(const size_type pos) {
+    if (pos >= size_) {
+      throw std::out_of_range("Tryin to access element which is out of range");
+    }
+    return *to_value_type_pointer(storage_ + pos);
+  }
+
+  const_reference operator[](const size_type pos) const noexcept {
+    assert(pos < size_);
+    // note: needs std::launder as of C++17
+    return *to_value_type_pointer(storage_ + pos);
+  }
+
+  reference operator[](const size_type pos) noexcept {
+    assert(pos < size_);
+    // note: needs std::launder as of C++17
+    return *to_value_type_pointer(storage_ + pos);
+  }
+
+  reference front() noexcept { return *to_value_type_pointer(storage_); }
+
+  const_reference front() const noexcept {
+    return *to_value_type_pointer(storage_);
+  }
+
+  reference back() noexcept {
+    return *to_value_type_pointer(storage_ + size_ - 1);
+  }
+
+  const_reference back() const noexcept {
+    return *to_value_type_pointer(storage_ + size_ - 1);
+  }
+
+  pointer data() noexcept { return to_value_type_pointer(storage_); }
+
+  const_pointer data() const noexcept {
+    return to_value_type_pointer(storage_);
+  }
+
+  iterator begin() noexcept {
+    return iterator{to_value_type_pointer(storage_)};
+  }
+
+  iterator end() noexcept {
+    return iterator{to_value_type_pointer(storage_ + size_)};
+  }
+
+  bool empty() const noexcept { return size_ == 0; }
+
+  size_type size() const noexcept { return size_; }
+
+  size_type max_size() const noexcept { return size_; }
+
+  ~heap_array() { reset_storage(); }
 };
 
 } // namespace vlrx
--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -7,6 +7,7 @@ TEST_CASE("It is possible to create heap_array by providing size",
           "[construction][uninitialized]") {
   const vlrx::heap_array<int> test_array(5);
   REQUIRE(test_array.size() == 5);
+  REQUIRE(test_array.max_size() == 5);
 }
 
 TEST_CASE("It is possible to create heap_array from initializer list",
@@ -26,4 +27,66 @@ TEST_CASE("It is possible to modify data via subscript operator",
   REQUIRE(test_array.size() == 5);
   test_array[4] = 3;
   REQUIRE(test_array[4] == 3);
+}
+
+TEST_CASE("Checked access throws on out of bounds access",
+          "[checked access][out of bounds]") {
+  const vlrx::heap_array<int> test_array{1, 2, 3};
+  REQUIRE(test_array.at(2) == 3);
+  REQUIRE_THROWS(test_array.at(3));
+}
+
+TEST_CASE("Front and back on non-empty containers return proper data",
+          "[front][back]") {
+  const vlrx::heap_array<int> test_array{1, 2, 3};
+  REQUIRE(test_array.front() == 1);
+  REQUIRE(test_array.back() == 3);
+}
+
+TEST_CASE(
+    "Empty for non-empty containers returns false and for empty returns true",
+    "[empty]") {
+  const vlrx::heap_array<int> test_array{1, 2, 3};
+  REQUIRE(test_array.empty() == false);
+  const vlrx::heap_array<int> test_array1(0);
+  REQUIRE(test_array1.empty() == true);
+}
+
+TEST_CASE("Data for non-empty containers returns pointer to the 0-th element",
+          "[data]") {
+  vlrx::heap_array<int> test_array{1, 2, 3};
+  REQUIRE(test_array.data() != nullptr);
+  test_array.data()[1] = 3;
+  REQUIRE(test_array[1] == 3);
+}
+
+TEST_CASE("Begin and end return proper iterators, iterators are incrementable",
+          "[iterators][begin][end][increment]") {
+  vlrx::heap_array<int> test_array{1, 2, 3};
+  auto iter = test_array.begin();
+  REQUIRE(*iter == 1);
+  ++iter;
+  ++iter;
+  ++iter;
+  REQUIRE(iter == test_array.end());
+}
+
+TEST_CASE("It is possible to add or substract number from the iterator to get "
+          "another iterator. It is possible to substract iterators",
+          "[iterators][arithmetic]") {
+  vlrx::heap_array<int> test_array{1, 2, 3, 4, 5};
+  auto iter = test_array.begin();
+  auto iter1 = iter + 2;
+  REQUIRE(*iter1 == 3);
+  iter1 = 2 + iter1;
+  REQUIRE(*iter1 == 5);
+  iter1 = iter1 - 1;
+  REQUIRE(*iter1 == 4);
+  iter1 += 1;
+  REQUIRE(*iter1 == 5);
+  iter1 -= 1;
+  REQUIRE(*iter1 == 4);
+  const auto iter2 = test_array.begin();
+  REQUIRE(iter2[2] == 3);
+  REQUIRE(test_array.end() - test_array.begin() == 5);
 }
